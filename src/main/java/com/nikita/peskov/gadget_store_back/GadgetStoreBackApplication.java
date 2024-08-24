8e233951424514074002cf00f3e749ec81959f4e
nikita <pesochek2019@gmail.com>
2024-08-24 19:40:17 +0300

    init project

---
+++ src/main/java/com/nikita/peskov/gadget_store_back/GadgetStoreBackApplication.java
@@ -0,0 +1,13 @@
+package com.nikita.peskov.gadget_store_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetStoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GadgetStoreBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikita/peskov/gadget_store_back/GadgetStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikita.peskov.gadget_store_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
